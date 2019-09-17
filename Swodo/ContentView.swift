fc3f82b12c10be5eab409ce13972f65ba73147a5
Oschly <iriusturar@icloud.com>
2019-09-17 06:08:58 +0200

    Initial Commit

---
+++ Swodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swodo
+//
+//  Created by Oschły on 17/09/2019.
+//  Copyright © 2019 Oschły. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
