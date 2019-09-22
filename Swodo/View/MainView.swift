fb0424d55e0efb8c57e27e6775598ba6e7e3b98c
Oschly <iriusturar@icloud.com>
2019-09-22 22:05:57 +0200

    Update body of mainView
    Why:
    - using timer allows us to stop progress ring and read its actual value
    
    How:
    - delete withAnimation and on .animation which behavior is defined by stopAnimation boolean.
    - Start button creates scheduledTimer which decreases fillPoint by dividing actual animationDuration by absolute duration, then decrease animationDuration
    - Stop button invalidates countdownTimer and toggles stopAnimation to true

--- Swodo/View/MainView.swift
+++ Swodo/View/MainView.swift
@@ -10,36 +10,53 @@ import SwiftUI
 
 struct MainView: View {
   @State private var fillPoint = 1.0
-  @State private var animationDuration = 5.0
+  @State private var animationDuration = 60.0
+  @State private var  stopAnimation = true
+  @State private var countdownTimer: Timer?
   
+  private var fillPointMax: Double = 60
+  
+  private var ring: Ring {
+    let ring = Ring(fillPoint: self.fillPoint)
+    return ring
+  }
+  
+  // https://stackoverflow.com/a/58048635/8140676
   var body: some View {
     VStack {
-      Ring(fillPoint: fillPoint).stroke(Color.red, lineWidth: 15.0)
+      ring.stroke(Color.red, lineWidth: 15.0)
         .frame(width: 200, height: 200)
         .padding(40)
+        .animation(self.stopAnimation ? nil : .easeIn(duration: 0.1))
       HStack {
         Button(action: {
-          withAnimation {
-              self.fillPoint = 0
-          }
+          guard self.stopAnimation == true else { return }
+          self.stopAnimation = false
+          self.countdownTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { _ in
+            guard self.animationDuration > 0 else {
+              self.countdownTimer?.invalidate()
+              return
+            }
+            self.fillPoint = self.animationDuration/self.fillPointMax
+            self.animationDuration -= 0.1
+            print(self.fillPoint)
+          })
         }) {
           Text("Start")
         }
         Button(action: {
-          withAnimation { 
-            self.fillPoint = 1
-          }
+          self.countdownTimer?.invalidate()
+          self.stopAnimation = true
         }) {
-          Text("Reset")
+          Text("Stop")
         }
       }
-      
     }
   }
 }
-
 struct MainView_Previews: PreviewProvider {
   static var previews: some View {
     MainView()
   }
 }
+
