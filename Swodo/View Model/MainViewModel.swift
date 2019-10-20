a0196e557cde8d710451e7404c457a46ff7e47dd
Oschly <iriusturar@icloud.com>
2019-10-20 21:22:17 +0200

    Make switchable views partly working.
    - delete useless code
    - move most `some View`s to mainView to easier manage their bindings
    - move few methods and data from MainViewModel to MainView to keep their jobs theirs.

--- Swodo/View Model/MainViewModel.swift
+++ Swodo/View Model/MainViewModel.swift
@@ -21,13 +21,11 @@ enum TimerState {
 final class MainViewModel: ObservableObject {
   var didChange = PassthroughSubject<Void, Never>()
   
-  // This object allows us to control anim
+  // Ring's animation data
   @Published var countdownTimer: Timer? { didSet { didChange.send() } }
-  
-  // Overall ring's fill progress
   @Published var progressValue = 1.0 { didSet { didChange.send() } }
   @Published var animationDuration = 5.0 { didSet { didChange.send() } }
-  @Published var shouldAnimate = true { didSet { didChange.send() } }
+  @Published var isAnimationStopped = true { didSet { didChange.send() } }
   @Published var workTime = 1 {
     didSet {
       self.animationDuration = Double(workTime * 5)
@@ -39,23 +37,14 @@ final class MainViewModel: ObservableObject {
   @Published var numberOfSessions = 1 { didSet { didChange.send() } }
   @Published var state: TimerState = .notStarted { didSet { didChange.send() } }
   
-  private var newRing = AnimatedRing()
-  
-  // MARK: - Views
-  var ring: some View {
-    AnimatedRing(progressValue: progressValue, shouldAnimate: shouldAnimate)
-  }
-  
-  var pickers: some View {
-    Pickers(duration: progressValue, sessions: numberOfSessions)
-  }
-  
+  // https://stackoverflow.com/a/58048635/8140676
   func startAnimation() {
-    guard shouldAnimate else { return }
-    shouldAnimate = false
-    countdownTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { _ in
+    guard isAnimationStopped else { return }
+    isAnimationStopped = false
+    countdownTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: {  _ in
       guard self.animationDuration > 0 else {
         self.countdownTimer?.invalidate()
+        self.state = .endOfWork
         return
       }
       self.progressValue = self.animationDuration/Double(self.workTime * 5)
@@ -63,44 +52,10 @@ final class MainViewModel: ObservableObject {
     })
   }
   
-  func stopAnimation() {
-    self.countdownTimer?.invalidate()
-    self.shouldAnimate = true
+    func stopAnimation() {
+      countdownTimer?.invalidate()
+      isAnimationStopped = true
   }
   
-  func returnProperView() -> AnyView {
-    switch state {
-    case .notStarted:
-      return AnyView(Pickers(duration: self.animationDuration, sessions: self.numberOfSessions))
-    case .paused:
-      return AnyView(pickers)
-    case .workTime:
-      defer {
-        startAnimation()
-      }
-      
-      return AnyView(ring)
-    case .endOfWork:
-      return AnyView(pickers)
-    case .breakTime:
-      return AnyView(ring)
-    case .endOfBreak:
-      return AnyView(ring)
-    }
-  }
-}
 
-struct AnimatedRing: View {
-  
-  @State var progressValue: Double = 1.0
-  @State var shouldAnimate: Bool = true
-  
-  #warning("Add Feature to change color of Ring")
-  var body: some View {
-    Ring(fillPoint: 0.0)
-      .stroke(Color.red, lineWidth: 15.0)
-      .frame(width: 200, height: 200)
-      .padding(40)
-      .animation(shouldAnimate ? nil : .easeIn(duration: 0.1))
-  }
 }
--- Swodo/View/MainView.swift
+++ Swodo/View/MainView.swift
@@ -11,24 +11,86 @@ import SwiftUI
 struct MainView: View {
   @ObservedObject private var viewModel = MainViewModel()
   
-  // https://stackoverflow.com/a/58048635/8140676
+  private var pickers: some View {
+    GeometryReader { geometry in
+      HStack {
+        VStack {
+          Picker(selection: self.$viewModel.workTime, label: Text("")) {
+            ForEach(Range(1...24), id: \.self) { index in
+              Text("\(index * 5) Minutes").id(index)
+            }
+          }.frame(maxWidth: geometry.size.width / 2,
+                  maxHeight: 74)
+            .clipped()
+            .labelsHidden()
+          Text("Session's duration")
+        }
+        VStack {
+          Picker(selection: self.$viewModel.numberOfSessions,
+                 label: Text("Number of sessions")) {
+                  ForEach(Range(1...10), id: \.self) { index in
+                    Text("\(index)").id(index)
+                  }
+          }.frame(maxWidth: geometry.size.width / 2,
+                  maxHeight: 74)
+            .clipped()
+            .labelsHidden()
+          Text("Number of sessions")
+        }
+      }
+    }
+  }
+  
+  private var ring: some View {
+    return Ring(fillPoint: viewModel.progressValue)
+      .stroke(Color.red, lineWidth: 15.0)
+      .frame(width: 200, height: 200)
+      .padding(40)
+      .animation(viewModel.isAnimationStopped ? nil : .easeIn(duration: 0.1))
+  }
+  
   var body: some View {
     VStack {
-      self.viewModel.returnProperView()
+      returnProperView()
       HStack {
         Button(action: {
-          self.viewModel.startAnimation()
+          self.viewModel.state = .workTime
         }) {
           Text("Start")
         }
         Button(action: {
-          self.viewModel.stopAnimation()
+          self.viewModel.state = .paused
         }) {
           Text("Stop")
         }
       }
     }
   }
+  
+  internal func returnProperView() -> AnyView {
+    switch viewModel.state {
+    case .notStarted:
+      return AnyView(pickers)
+    case .paused:
+      defer {
+        viewModel.stopAnimation()
+      }
+      return AnyView(pickers)
+    case .workTime:
+      defer {
+        viewModel.startAnimation()
+      }
+      return AnyView(ring)
+    case .endOfWork:
+      return AnyView(pickers)
+    case .breakTime:
+      return AnyView(ring)
+    case .endOfBreak:
+      return AnyView(ring)
+    }
+  }
+  
+  
 }
 struct MainView_Previews: PreviewProvider {
   static var previews: some View {
