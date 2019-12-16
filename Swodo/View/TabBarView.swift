6e54bc7a18f31e1668f40e75f785b95889163b08
Oschly <iriusturar@icloud.com>
2019-12-16 19:00:10 +0100

    Add animation for appearing views

--- Swodo/View/TabBarView.swift
+++ Swodo/View/TabBarView.swift
@@ -10,40 +10,56 @@ import SwiftUI
 
 struct TabBarView: View {
   @ObservedObject var mainViewModel: MainViewModel
+  @State private var opacity = 0.0
   
   var body: some View {
     if mainViewModel.state == .workTime {
-      return AnyView(MainView(viewModel: mainViewModel))
-    }
-    return withAnimation {
-      AnyView(TabView {
-        MainView(viewModel: mainViewModel).tabItem {
-          VStack {
-            Text("Timer")
-            Image(systemName: "clock")
-              .font(.system(size: 23))
+      return AnyView(MainView(viewModel: mainViewModel)
+        .opacity(opacity)
+        .onAppear {
+          withAnimation(.easeIn) {
+            self.opacity = 1.0
           }
-        }
-        
-        Text("Stats").tabItem {
-          VStack {
-            Text("Statistics")
-            Image(systemName: "chart.bar")
-              .font(.system(size: 23))
-          }
-        }
-        
-        Text("Settings").tabItem {
-          VStack {
-            Text("Settings")
-            Image(systemName: "gear")
-              .font(.system(size: 23))
-            
-          }
-        }
+      }
+      .onDisappear {
+        self.opacity = 0.0
       })
     }
+    
+    return AnyView(TabView {
+      MainView(viewModel: mainViewModel).tabItem {
+        VStack {
+          Text("Timer")
+          Image(systemName: "clock")
+            .font(.system(size: 23))
+        }
+      }
+      
+      Text("Stats").tabItem {
+        VStack {
+          Text("Statistics")
+          Image(systemName: "chart.bar")
+            .font(.system(size: 23))
+        }
+      }
+      
+      Text("Settings").tabItem {
+        VStack {
+          Text("Settings")
+          Image(systemName: "gear")
+            .font(.system(size: 23))
+          
+        }
+      }
+    }
+    .opacity(opacity)
+    .onAppear {
+      withAnimation(.easeIn) {
+        self.opacity = 1.0
+      }
+    }
+    .onDisappear {
+      self.opacity = 0.0
+    })
   }
-  
-  
 }
